91cf6836aab8dd3af82da2bf3fcd4a4598fb4d07
Takis_tk <takistsekos@outlook.com>
2026-05-20 19:22:39 +0300

    getting user input

--- src/gr/aueb/cf/ch10/Student.java
+++ src/gr/aueb/cf/ch10/Student.java
@@ -1,7 +1,31 @@
 package gr.aueb.cf.ch10;
 
 public class Student {
-    public int id;
-    public  String firstname;
-    public String lastname;
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
