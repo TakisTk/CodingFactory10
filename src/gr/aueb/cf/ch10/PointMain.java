13f442517e4de2bbac6effe1e8eef425f449ed43
Takis_tk <takistsekos@outlook.com>
2026-05-20 20:59:49 +0300

    chore: Update greeting message and modify .gitnore

---
+++ src/gr/aueb/cf/ch10/PointMain.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch10;
+
+import gr.aueb.cf.ch10.model.Point;
+
+public class PointMain {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(5, 10);
+
+
+    }
+}
