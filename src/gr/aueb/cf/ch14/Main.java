58b72951c7054d7eb7f7f515057efc9986e8a8ea
Takis_tk <takistsekos@outlook.com>
2026-05-26 20:58:54 +0300

    chore: Point 3D

--- src/gr/aueb/cf/ch14/Main.java
+++ src/gr/aueb/cf/ch14/Main.java
@@ -9,6 +9,10 @@ public class Main {
         Point p4 = new Point2D(15.1, 20.2);
         Point p5 = new Point3D(15.1, 20.2, 30.3);
 
+//        System.out.println(p1);
+//        System.out.println(p2);
+//        System.out.println(p3);
+
         p1.movePlusOne();
         p2.movePlusOne();
         p3.movePlusOne();
@@ -18,10 +22,20 @@ public class Main {
         doMovePlusOne(p3);
         doMovePlusOne(p4);
         doMovePlusOne(p5);
+
+        doPrint(p1);
+        doPrint(p2);
+        doPrint(p3);
+        doPrint(p4);
+        doPrint(p5);
     }
 
     // Polymorphism
     public static void doMovePlusOne(Point p) {
         p.movePlusOne();      // late binding
+    }
+
+    public static void doPrint(Point p) {
+        System.out.println(p);
     }
 }
