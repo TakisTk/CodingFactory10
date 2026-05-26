888ae631162feb694ed863361e6f6f762f549c33
Takis_tk <takistsekos@outlook.com>
2026-05-26 18:53:02 +0300

    chore: Update greeting message and modify .gitnore

--- src/gr/aueb/cf/ch13/Main.java
+++ src/gr/aueb/cf/ch13/Main.java
@@ -3,6 +3,8 @@ package gr.aueb.cf.ch13;
 public class Main {
 
     static void main() {
+        OnlyOneCodingFactory onlyOneCodingFactory = OnlyOneCodingFactory.getInstance();
+        onlyOneCodingFactory.doSomething();
 
         //Καλούμε την υπηρεσία της utility class
         HelloUtil.sayHello();
