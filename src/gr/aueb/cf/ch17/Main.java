8d27f8993ef81e1392f2d10f60441bb0f5145e94
Takis_tk <takistsekos@outlook.com>
2026-06-04 20:36:53 +0300

    chore:

--- src/gr/aueb/cf/ch17/Main.java
+++ src/gr/aueb/cf/ch17/Main.java
@@ -3,9 +3,9 @@ package gr.aueb.cf.ch17;
 public class Main {
     static void main() {
         FlexibleNode flexibleNode = new FlexibleNode("Coding");
-
         int value = (int) flexibleNode.getValue();
-        System.out.println("Value: " + value);
+
+        System.out.println(value);
     }
 
 }
