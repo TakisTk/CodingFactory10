1c35a105703cc856cfae6c6f3a82eaa019b9374a
Takis_tk <takistsekos@outlook.com>
2026-06-04 20:20:27 +0300

    chore:

---
+++ src/gr/aueb/cf/ch17/FlexibleNode.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch17;
+
+public class FlexibleNode {
+    private Object value;
+
+    public FlexibleNode(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
