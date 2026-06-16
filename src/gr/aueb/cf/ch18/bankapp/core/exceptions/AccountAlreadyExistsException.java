4fe732da28fd922227eb47fb6b5c455273e8b33c
Takis_tk <takistsekos@outlook.com>
2026-06-16 21:05:52 +0300

    chore:

---
+++ src/gr/aueb/cf/ch18/bankapp/core/exceptions/AccountAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch18.bankapp.core.exceptions;
+
+public class AccountAlreadyExistsException extends Exception {
+    public AccountAlreadyExistsException(String message) {
+        super(message);
+    }
+}
