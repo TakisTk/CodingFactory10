564ae1eceb9066b83f74e25a2204aac65fa65265
Takis_tk <takistsekos@outlook.com>
2026-06-29 22:16:44 +0300

    chore:

--- src/gr/aueb/cf/ch19/DateMatcher.java
+++ src/gr/aueb/cf/ch19/DateMatcher.java
@@ -3,16 +3,21 @@ package gr.aueb.cf.ch19;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * The input string includes dates in the US format mm/dd/yyyy
+ * The program recognizes all the dates in the string and transforms
+ * them to GR format dd/mm/yyyy.
+ */
 public class DateMatcher {
 
-    static void main() {
-        String dates = "06/12/2020 xxx yyy ;;;;; 11/05/1999cxxxxxxx02/27/2000, 12/12/2025";
+    public static void main(String[] args) {
+        String dates = "06/30/2020 xxx yyy ;;;; 11/15/1999cxxxxxx02/27/2000, 10/12/2025";
 
         Pattern pattern = Pattern.compile("(\\d{2})/(\\d{2})/(\\d{4})");
         Matcher matcher = pattern.matcher(dates);
 
         while (matcher.find()) {
-            System.out.println(matcher.group());
+            System.out.println(matcher.group());    // όλο το match
 
             String month = matcher.group(1);
             String day = matcher.group(2);
@@ -20,6 +25,7 @@ public class DateMatcher {
 
             String grDate = day + "/" + month + "/" + year;
             System.out.println(grDate);
+            System.out.println();
         }
     }
 }
